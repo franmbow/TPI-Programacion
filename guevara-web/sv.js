01ffc65a7ead0176fd51a367d8930a2ec3646532
franmbow <frncbdl@gmail.com>
2025-10-10 22:41:25 -0300

    conexion exportada, Router de express importado

--- guevara-web/sv.js
+++ guevara-web/sv.js
@@ -1,25 +1,28 @@
 import express from 'express';
 import cors from 'cors';
 import mysql from 'mysql';
+import { signRouter } from './routes/signRouter.js';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-const conexion = mysql.createConnection({
+export const db = mysql.createPool({
     host: 'localhost',
     user: 'root',
     password: '',
     database: 'guevara'
 });
 
-conexion.connect((error) => {
+db.getConnection((error) => {
     if (error) {
         console.error('Error de conexión: ', error);
 
     } else {
         console.log('Conectado a la base de datos');
 }});
+
+app.use('/', signRouter);
 
 app.listen(3000, () => {
     console.log('Servidor escuchando en el puerto 3000');
