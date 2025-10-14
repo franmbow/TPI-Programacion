51776358ad68d75289be69b8367d80e3d11dbefd
franmbow <frncbdl@gmail.com>
2025-10-13 22:16:30 -0300

    log fetch problemas

---
+++ guevara-web/log.js
@@ -0,0 +1,26 @@
+document.getElementById('logForm').addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const userNombre = document.getElementById('usuario').value.trim();
+    const contraseña = document.getElementById('contraseña').value.trim();
+
+    try {
+        const respuesta = await fetch('http://localhost:3000/log', {
+            method: 'POST',
+            headers: { "Content-Type" : 'application/json' },
+            body: JSON.stringify({ userNombre, contraseña})
+        })
+        if (!respuesta.ok) {
+            const errorData = await respuesta.json();
+            throw new Error(errorData.mensaje || "Error al iniciar sesion");
+        }
+        const resultado = await respuesta.json();
+        console.log("user", resultado);
+        error.msg.textContent = "";
+        if (respuesta.status === 200) {
+            localStorage.setItem('token', resultado.token);
+            window.location.href = 'index.html';
+        }
+    } catch (error) {
+        console.error("Error:", error);
+    }
+})
