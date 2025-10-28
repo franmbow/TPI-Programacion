26d6e6fe79e7eb074472c95268f1a207f618db5c
franmbow <frncbdl@gmail.com>
2025-10-28 11:27:49 -0300

    Routers y session

--- guevara-web/sv.js
+++ guevara-web/sv.js
@@ -1,13 +1,22 @@
 import express from 'express';
+import session from 'express-session';
 import cors from 'cors';
 import mysql from 'mysql';
 import { signRouter } from './routes/signRouter.js';
 import { logRouter } from './routes/logRouter.js';
+import { notasRouter } from './routes/notasRouter.js';
 import { panelRouter } from './routes/panelRouter.js';
+import { userRouter } from './routes/userRouter.js';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use(session({
+    secret: '473862',
+    resave: false,
+    saveUninitialized: true,
+}));
 
 export const db = mysql.createPool({
     host: 'localhost',
@@ -26,7 +35,10 @@ db.getConnection((error) => {
 
 app.use('/', signRouter);
 app.use('/', logRouter);
+app.use('/', notasRouter);
 app.use('/', panelRouter);
+app.use('/', userRouter);
+
 
 app.listen(3000, () => {
     console.log('Servidor escuchando en el puerto 3000');
